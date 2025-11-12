f97db2b2bc7f76f336d9922fad9d433612eddddb
LNX <thilienluu.011991@gmail.com>
2025-11-12 09:43:56 +0700

    Add files via upload

---
+++ n.py
@@ -0,0 +1,18 @@
+import datetime 
+# Người dùng nhập ngày sinh
+birth_str = input("15")
+
+# Chuyển chuỗi thành đối tượng ngày tháng
+birth_date = datetime.strptime(birth_str, "28/03/2010")
+
+# Lấy ngày hôm nay
+today = date.today()
+
+# Tính tuổi (năm hiện tại - năm sinh)
+age = today.year - birth_date.year
+
+# Nếu chưa tới sinh nhật năm nay thì trừ đi 1
+if (today.month, today.day) < (birth_date.month, birth_date.day):
+    age -= 1
+
+print(f"Tuổi của bạn là: {15}")
